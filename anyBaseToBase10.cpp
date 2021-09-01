bf1f010c97314b70b8dc3476c63ad994bf0e277d
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-09-01 15:03:30 -0700

    Changed my mind, will only have one branch

---
+++ anyBaseToBase10.cpp
@@ -0,0 +1,5 @@
+long anyBaseToBase10(const char* input_number, unsigned number_size,
+    unsigned input_base)
+{
+
+}
---
+++ anyBaseToBase10.h
@@ -0,0 +1,10 @@
+#ifndef ANYBASE_TO_BASE10
+#define ANYBASE_TO_BASE10
+
+//number_size is the number of characters that input_number
+//is, aka what we would expect strlen() to return assuming
+//that input_number does have a null terminating character
+long anyBaseToBase10(const char* input_number, unsigned number_size,
+    unsigned input_base);
+
+#endif //ANYBASE_TO_BASE10
